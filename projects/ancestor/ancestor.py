da18e34898a5ca611aaa3da4de54ab5010447199
tigju <tigju24@gmail.com>
2020-11-18 21:25:22 -0500

    The earliest ancestor assignment

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -42,37 +42,8 @@ Adjecency list looks like this:
 
 from collections import deque
 
-def earliest_ancestor(ancestors, starting_node):
-    
-    stack = deque()
-    visited = set()
-
-    stack.append(starting_node)
-
-    while len(stack) > 0:
-        curr_node = stack.pop()
-    
-        if curr_node not in visited:
-            visited.add(curr_node)
-
-            for ancestor in ancestors:
-                if curr_node == ancestor[1]:
-                    stack.append(ancestor[0])
-    
-    if curr_node == starting_node:
-        curr_node = -1
-                
-    return curr_node
-
-# solution with the graph building
+# solution without graph building
 # def earliest_ancestor(ancestors, starting_node):
-#     ancestors_dict = {}
-#     for ancestor in ancestors:
-#         if ancestor[1] in ancestors_dict:
-#             ancestors_dict[ancestor[1]].add(ancestor[0])
-#         else:
-#             ancestors_dict[ancestor[1]] = set()
-#             ancestors_dict[ancestor[1]].add(ancestor[0])
     
 #     stack = deque()
 #     visited = set()
@@ -81,18 +52,48 @@ def earliest_ancestor(ancestors, starting_node):
 
 #     while len(stack) > 0:
 #         curr_node = stack.pop()
-
+    
 #         if curr_node not in visited:
 #             visited.add(curr_node)
 
-#             for anc in ancestors_dict:
-
-#                 if curr_node == anc:
-#                     curr_node = min(ancestors_dict[anc])
-#                     stack.append(curr_node)
+#             for ancestor in ancestors:
+#                 if curr_node == ancestor[1]:
+#                     stack.append(ancestor[0])
+    
 #     if curr_node == starting_node:
 #         curr_node = -1
+                
 #     return curr_node
+
+# solution with the graph building
+def earliest_ancestor(ancestors, starting_node):
+    ancestors_dict = {}
+    for ancestor in ancestors:
+        if ancestor[1] in ancestors_dict:
+            ancestors_dict[ancestor[1]].add(ancestor[0])
+        else:
+            ancestors_dict[ancestor[1]] = set()
+            ancestors_dict[ancestor[1]].add(ancestor[0])
+    
+    stack = deque()
+    visited = set()
+
+    stack.append(starting_node)
+
+    while len(stack) > 0:
+        curr_node = stack.pop()
+
+        if curr_node not in visited:
+            visited.add(curr_node)
+
+            for anc in ancestors_dict:
+
+                if curr_node == anc:
+                    curr_node = min(ancestors_dict[anc])
+                    stack.append(curr_node)
+    if curr_node == starting_node:
+        curr_node = -1
+    return curr_node
 
 pairs = [(1,3), (2,3), (3,6), (5,6), (5,7), (4,5), (4,8), (8,9), (11,8), (10,1)]
 
